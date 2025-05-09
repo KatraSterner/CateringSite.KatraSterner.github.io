3cd9a0f31c8a05e44abcc11af3679a910982ecae
Katra Sterner <Rollie406@gmail.com>
2025-05-08 22:33:44 -0500

    files uploaded without root directory

---
+++ script/item.js
@@ -0,0 +1,9 @@
+class Item {
+    constructor(image, name, one, two, three, four) {
+        this.image = image;
+        this.name = name;
+        this.list = [one, two, three, four];
+    }
+}
+
+export default Item;
