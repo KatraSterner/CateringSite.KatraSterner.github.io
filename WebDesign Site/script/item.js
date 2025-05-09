ecd90de5e6a97f413e1c9d722f6a7453dbb0d2a2
Katra Sterner <Rollie406@gmail.com>
2025-05-08 22:32:57 -0500

    Delete WebDesign Site directory

--- WebDesign Site/script/item.js
+++
@@ -1,9 +0,0 @@
-class Item {
-    constructor(image, name, one, two, three, four) {
-        this.image = image;
-        this.name = name;
-        this.list = [one, two, three, four];
-    }
-}
-
-export default Item;
